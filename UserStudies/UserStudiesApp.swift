90cb36b80aa0b59b2d43044cc67e56b957c40049
Hannah Farley <hannah.m.farley@vanderbilt.edu>
2023-04-02 14:47:54 -0500

    Initial Commit

---
+++ UserStudies/UserStudiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserStudiesApp.swift
+//  UserStudies
+//
+//  Created by Hannah Farley on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct UserStudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
