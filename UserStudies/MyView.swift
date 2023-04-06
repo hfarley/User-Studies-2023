759b119c9747656e21e218203461901f7f24bb35
Hannah Farley <hannah.m.farley@vanderbilt.edu>
2023-04-06 17:03:32 -0500

    changed compatibility

--- UserStudies/MyView.swift
+++ UserStudies/MyView.swift
@@ -4,7 +4,7 @@
 //This task is about debugging. First fix the error in the MyView code, then modify the MyView file to give the expected behavior. MyView should display a button that says “Show Blue Screen,” that when clicked by a user, displays the view called Blue (also included in the project). Do not change the contents of the Blue file.
 
 
-import SwiftUI
+//import SwiftUI
 
 //struct MyView: View {
 //    var body: some View {
